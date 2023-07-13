958da090e4ef66b6305ee9ca9f9663ab2e91b6fe
月小泪 <york.yang.me@gmail.com>
2023-07-13 20:55:47 +0900

    add tailwind style files

--- preact-app/src/app.tsx
+++ preact-app/src/app.tsx
@@ -7,7 +7,9 @@ export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <><h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} class="logo" alt="Vite logo" />
